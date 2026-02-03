b4b7a78540ba525e4c41ea1ee050d9b709e66b91
Fyced <victor.frigols.planells@gmail.com>
2026-02-03 11:57:01 +0100

    1.TrySolveService

--- service-worker.js
+++ service-worker.js
@@ -15,6 +15,26 @@ self.addEventListener("install", event => {
   );
 });
 
+// Activación: AQUÍ BORRAMOS LA CACHÉ VIEJA (v1)
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            console.log("Borrando caché antigua: " + cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  // Reclama el control de los clientes inmediatamente
+  return self.clients.claim();
+});
+
 // Interceptar peticiones y responder desde el cache si existe
 self.addEventListener("fetch", event => {
   event.respondWith(
